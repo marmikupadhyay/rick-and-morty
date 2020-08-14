641c11f025017721a4bfdd422cf59b9c0b9fbc2c
Marmik Upadhyay <marmikupadhyay.mu@gmail.com>
2020-08-14 15:30:02 +0530

    Commit-3: Changed BrowserRouter to HashRouter.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
 import Header from "./components/Header";
@@ -11,7 +11,7 @@ import EpisodesContainer from "./components/EpisodesContainer";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <React.Fragment>
         <Header />
         <Route
