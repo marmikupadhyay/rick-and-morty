1380e7ab7995c88903ba3db7150ae8bbdc710f3e
Marmik Upadhyay <marmikupadhyay.mu@gmail.com>
2020-08-14 15:22:41 +0530

    Updates

---
+++ precache-manifest.c0d7f64bc8c37f3820e50475b89b29a4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47371bfc245711c9ada174558319110e",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "e66445c11fa645e9c876",
+    "url": "/rick-and-morty/static/css/main.5ad902a8.chunk.css"
+  },
+  {
+    "revision": "da7d1f8b8b0a8a3b7205",
+    "url": "/rick-and-morty/static/js/2.e6121905.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-and-morty/static/js/2.e6121905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e66445c11fa645e9c876",
+    "url": "/rick-and-morty/static/js/main.2baeb0fb.chunk.js"
+  },
+  {
+    "revision": "30e0f13fdd1f03db73d7",
+    "url": "/rick-and-morty/static/js/runtime-main.3ebd1b17.js"
+  },
+  {
+    "revision": "c9b9dba66cf4298a79006bfa1aa3f2e2",
+    "url": "/rick-and-morty/static/media/404.c9b9dba6.png"
+  },
+  {
+    "revision": "53514534e9427d7099646b8474d895a8",
+    "url": "/rick-and-morty/static/media/Characters.53514534.png"
+  },
+  {
+    "revision": "6f67ea928bc12a1e63ae4c0c52a09ec7",
+    "url": "/rick-and-morty/static/media/Episodes.6f67ea92.png"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "/rick-and-morty/static/media/Heading.75509f2e.png"
+  },
+  {
+    "revision": "b169425d31b87126a32e69873d2122a4",
+    "url": "/rick-and-morty/static/media/Location1.b169425d.png"
+  },
+  {
+    "revision": "2ba7e0b80bc279533c6a887a6bdca8fa",
+    "url": "/rick-and-morty/static/media/Portal.2ba7e0b8.png"
+  },
+  {
+    "revision": "4b4420cffe3ba4f13221e529e5b60078",
+    "url": "/rick-and-morty/static/media/siteFont.4b4420cf.ttf"
+  }
+]);
